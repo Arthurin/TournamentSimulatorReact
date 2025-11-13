a88b8d7abe5e98aec3e343848edf9e87693f3ed2
Arthurin <6535135+Arthurin@users.noreply.github.com>
2025-11-13 23:30:10 +0100

    routeur pour github

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { V2 } from "./pages/V2";
 import { NotFound } from "./pages/NotFound";
-import BadmintonMatchMaker from "@/components/BadmintonMatchMaker";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/v2" element={<V2 />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter basename="/TournamentSimulatorReact">
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="v2" element={<V2 />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
